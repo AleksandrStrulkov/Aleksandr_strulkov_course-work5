d8782a63ca711e27ca3690ee0b7e1cb58c0e1561
AleksandrStrulkov <astrulkov@yandex.ru>
2023-11-10 00:12:29 +0300

    Correction DBManager.py

--- src/DBManager.py
+++ src/DBManager.py
@@ -1,6 +1,69 @@
-import database_creation
 import psycopg2
-import requests
+
+
+class CreateDatabase:
+    """Класс создания базы данных, таблицы и ее заполнения"""
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def create_database(params, db_name) -> None:
+        """
+        Функция создания базы данных
+        """
+
+        connection = psycopg2.connect(dbname='postgres', **params)
+        connection.autocommit = True
+        cursor = connection.cursor()
+
+        cursor.execute(f"DROP DATABASE IF EXISTS {db_name};")
+        cursor.execute(f"CREATE DATABASE {db_name};")
+
+        cursor.close()
+        connection.close()
+
+    @staticmethod
+    def create_table(cur) -> None:
+        """
+        Функция создания таблицы для ее последующего заполнения данными о вакансиях
+        """
+        cur.execute(
+                """
+                DROP TABLE IF EXISTS employer;
+                CREATE TABLE employer(
+                name_company varchar(50),
+                vacancy_id int,
+                company_id int,
+                name_vacancy varchar(100),
+                salary_from int,
+                city varchar(50),
+                url_vacancy character varying(50),
+                schedule varchar(50),
+                experience character varying(50),
+                url_company character varying(50)
+                )
+                """
+                )
+
+    @staticmethod
+    def add_data_to_table(cur, data: list[dict]) -> None:
+        """
+        Функция для заполнения таблицы базы данных
+        """
+        for vacancy in data:
+            cur.execute("""
+            INSERT INTO employer 
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            """, (vacancy['name_company'],
+                    vacancy['vacancy_id'],
+                    vacancy['company_id'],
+                    vacancy['name_vacancy'],
+                    vacancy['salary_from'],
+                    vacancy['city'],
+                    vacancy['url_vacancy'],
+                    vacancy['schedule'],
+                    vacancy['experience'],
+                    vacancy['url_company']))
 
 
 class DBManager:
@@ -9,77 +72,41 @@ class DBManager:
         self.params = params
 
     @staticmethod
-    def get_companies_and_vacancies_count(table_name, cur):
+    def get_companies_and_vacancies_count(cur):
         """Получает список всех компаний и количество вакансий у каждой компании"""
-        cur.execute(f'SELECT name_company, count(*) FROM {table_name} GROUP BY name_company')
+        cur.execute('SELECT name_company, count(*) FROM employer GROUP BY name_company')
         result = cur.fetchall()
 
         return result
 
     @staticmethod
-    def get_all_vacancies(table_name, cur):
+    def get_all_vacancies(cur):
         """Получает список всех вакансий с указанием названия компании, названия вакансии
         и зарплаты и ссылки на вакансию"""
-        cur.execute(f'SELECT * FROM {table_name}')
+        cur.execute("""SELECT * FROM employer""")
         result = cur.fetchall()
 
         return result
 
     @staticmethod
-    def get_avg_salary(table_name, cur):
+    def get_avg_salary(cur):
         """Получает среднюю зарплату по вакансиям"""
-        cur.execute(f'SELECT AVG(salary_from) as average, '
-                    f'FROM {table_name}')
+        cur.execute("""SELECT AVG(salary_from) as average
+                        FROM employer""")
         result = cur.fetchall()
         return result
 
     @staticmethod
-    def get_vacancies_with_higher_salary(table_name, cur):
+    def get_vacancies_with_higher_salary(cur):
         """Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям"""
-        cur.execute(f'SELECT * FROM {table_name} '
-                    f'WHERE salary_from > (SELECT AVG(salary_from) FROM {table_name})')
+        cur.execute("""SELECT * FROM employer
+                    WHERE salary_from > (SELECT AVG(salary_from) FROM employer)""")
         result = cur.fetchall()
         return result
 
     @staticmethod
-    def get_vacancies_with_keyword(table_name, keyword, cur):
+    def get_vacancies_with_keyword(keyword, cur):
         """Получает список всех вакансий, в названии которых содержатся переданные в метод слова, например “python”"""
-        cur.execute(f"SELECT * FROM {table_name} "
-                    f"WHERE name_vacancy LIKE '%{keyword}%'")
+        cur.execute(f"SELECT * FROM employer WHERE name_vacancy LIKE '%{keyword}%'")
         result = cur.fetchall()
         return result
-
-    def manager(self, key: str, table_name, keyword=None) -> list[tuple]:
-        """
-        Метод-менеджер для вызова других методов класса
-        :param key: ключ
-        :param table_name: название таблицы для обращения
-        :param keyword: ключевое слово для фильтрации
-        :return: результат работы соответствующего SQL запроса
-        """
-        conn = None
-        try:
-            with psycopg2.connect(**self.params) as conn:
-                with conn.cursor() as cur:
-
-                    if key == '1':
-                        rows = self.get_companies_and_vacancies_count(table_name, cur)
-
-                    elif key == '2':
-                        rows = self.get_all_vacancies(table_name, cur)
-
-                    elif key == '3':
-                        rows = self.get_avg_salary(table_name, cur)
-
-                    elif key == '4':
-                        rows = self.get_vacancies_with_higher_salary(table_name, cur)
-
-                    elif key == '5':
-                        rows = self.get_vacancies_with_keyword(table_name, keyword, cur)
-
-        except(Exception, psycopg2.DatabaseError) as error:
-            print(error)
-        finally:
-            if conn is not None:
-                conn.close()
-            return rows
